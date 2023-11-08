72a95213b062b51b81e7f4cf5cf72cafcd73a128
scharantej <119601237+scharantej@users.noreply.github.com>
2023-11-09 02:41:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
- The design of the registration application with email, first name, and last name is as follows:
-
-
+ 
 # Import the necessary modules
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
@@ -52,7 +50,7 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-The main.py file code that builds the flask application is as follows:
+main.py file:
 
 
 from flask import Flask, render_template, request, redirect, url_for
@@ -64,11 +62,11 @@ conn = sqlite3.connect('registrants.db')
 c = conn.cursor()
 
 c.execute('''CREATE TABLE IF NOT EXISTS registrants (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                first_name TEXT,
-                last_name TEXT,
-                email TEXT
-                )''')
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            first_name TEXT,
+            last_name TEXT,
+            email TEXT
+            )''')
 
 @app.route('/')
 def home():
@@ -94,43 +92,3 @@ def register():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-The HTML code for the index.html file is as follows:
-
-html
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Registration Application</title>
-</head>
-<body>
-    <h1>Welcome to the Registration Application</h1>
-    <p>Please enter your information to register.</p>
-    <form action="/register" method="post">
-        <label for="first_name">First Name:</label><br>
-        <input type="text" id="first_name" name="first_name"><br>
-        <label for="last_name">Last Name:</label><br>
-        <input type="text" id="last_name" name="last_name"><br>
-        <label for="email">Email:</label><br>
-        <input type="email" id="email" name="email"><br><br>
-        <input type="submit" value="Register">
-    </form>
-</body>
-</html>
-
-
-The HTML code for the register.html file is as follows:
-
-html
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Registration Application</title>
-</head>
-<body>
-    <h1>Registration Successful</h1>
-    <p>Thank you for registering. You will receive an email to confirm your attendance.</p>
-    <a href="/">Return to Home Page</a>
-</body>
-</html>
